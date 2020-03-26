b2fee652114d58d4db3499c8fd21c7751fc01a16
Kurt Madel <kmadel@cloudbees.com>
2020-03-26 11:59:09 -0400

    Update init_07_casc.groovy

--- quickstart/init_07_casc.groovy
+++ quickstart/init_07_casc.groovy
@@ -6,5 +6,5 @@ String scriptName = "init_07_casc.groovy"
 
 Logger logger = Logger.getLogger(scriptName)
 
-logger.info("attempting to load config as code from https://raw.githubusercontent.com/kypseli/demo-oc-casc/master/jcasc.yml")
-ConfigurationAsCode.get().configure("https://raw.githubusercontent.com/kypseli/demo-oc-casc/master/jcasc.yml")
+logger.info("attempting to load config as code from https://raw.githubusercontent.com/cloudbees-days/cb-core-oc-workshop/master/jcasc.yml")
+ConfigurationAsCode.get().configure("https://raw.githubusercontent.com/cloudbees-days/cb-core-oc-workshop/master/jcasc.yml")
